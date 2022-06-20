ddbee95a2653cf2748649aca5eee9e5d2f6e6362
Archisman-Mridha <archismanmridha12345@gmail.com>
2022-06-21 01:27:50 +0530

    implemented pattern matching

--- main.rs
+++ main.rs
@@ -4,29 +4,32 @@
 #![allow(unused_parens)]
 #![allow(unused_variables)]
 
-#[derive(Debug)]
+use core::fmt;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, PartialEq)]
 enum Expression {
 
     symbol(String),
     operation(String, Vec<Expression>)
 }
 
-impl Expression {
+impl fmt::Display for Expression {
 
-    fn prettyPrint(&self) {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Expression::symbol(name) => print!("{ }", name),
+            Expression::symbol(name) => write!(formatter, "{ }", name),
 
             Expression::operation(name, arguments) => {
-                print!("{ }(", name);
+                write!(formatter, "{ }(", name);
 
                 for (index, argument) in arguments.iter( ).enumerate( ) {
-                    if(index > 0) { print!(", "); }
+                    if(index > 0) { write!(formatter, ", "); }
 
-                    argument.prettyPrint( );
+                    write!(formatter, "{ }", argument);
                 }
 
-                print!(")");
+                return write!(formatter, ")");
             }
         }
     }
@@ -39,14 +42,10 @@ struct AxiomEquation {
     right: Expression
 }
 
-impl AxiomEquation {
+impl fmt::Display for AxiomEquation {
 
-    fn prettyPrint(&self) {
-        self.left.prettyPrint( );
-
-        print!(" = ");
-
-        self.right.prettyPrint( );
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        return write!(formatter, "{ } = { }", self.left, self.right);
     }
 }
 
@@ -57,16 +56,84 @@ impl AxiomEquation {
     }
 }
 
+fn matchPattern(referenceExpression: &Expression, providedExpression: &Expression) -> Option<HashMap<String, Expression>> {
+
+    fn recursiveHelper(referenceExpression: &Expression, providedExpression: &Expression, bindings: &mut HashMap<String, Expression>) -> bool {
+        use Expression::*;
+
+        match (referenceExpression, providedExpression) {
+
+            (symbol(name), _) => {
+                if let Some(existingSymbolValue)= bindings.get(name) {
+                    return existingSymbolValue == providedExpression;
+                }
+
+                else { bindings.insert(name.clone( ), providedExpression.clone( )); }
+
+                return true;
+            },
+    
+            (operation(referenceExpressionName, referenceExpressionArguments), operation(providedExpressionName, providedExpressionArguments)) => {
+                if(referenceExpressionName == providedExpressionName && referenceExpressionArguments.len( ) == providedExpressionArguments.len( )) {
+
+                    for index in 0..referenceExpressionArguments.len( ) {
+                        if(! recursiveHelper(&referenceExpressionArguments[index], &providedExpressionArguments[index], bindings)) {
+                            return false;
+                        }
+                    }
+
+                    return true;
+                }
+
+                else { return false; }
+            },
+
+            _ => { return false; }
+        }
+    }
+
+    let mut bindings: HashMap<String, Expression>= HashMap::new( );
+
+    if(recursiveHelper(referenceExpression, providedExpression, &mut bindings)) { return Some(bindings); }
+
+    else { return None; }
+}
+
 fn main( ) {
     use Expression::*;
 
-    // TODO: swap(pair(a, b))= pair(b, a)
+    // TODO: implement pattern matching
 
     let swapOperation= AxiomEquation {
 
-        left: operation(String::from("swap"), vec![ operation(String::from("pair"), vec![ symbol(String::from("a")), symbol(String::from("b")) ]) ]),
+        left: operation(
+            String::from("swap"), vec![ operation(
+                String::from("pair"), vec![ symbol(String::from("a")), symbol(String::from("b")) ]
+            )
+        ]),
         right: operation(String::from("pair"), vec![ symbol(String::from("b")), symbol(String::from("a")) ])
     };
 
-    swapOperation.prettyPrint( );
+    let referenceExpression= swapOperation.left;
+
+    println!("{ }", referenceExpression);
+
+    let providedExpression= operation(String::from("swap"), vec![
+        operation(String::from("pair"), vec![
+            operation(String::from("f"), vec![ symbol(String::from("g")) ]),
+            operation(String::from("f"), vec![ symbol(String::from("h")) ])
+        ])
+    ]);
+
+    println!("{ }", providedExpression);
+
+    if let Some(bindings)= matchPattern(&referenceExpression, &providedExpression) {
+        println!("pattern match successfull ! showing bindings : ");
+
+        for (key, value) in bindings.iter( ) {
+            println!("{ } : { }", key, value);
+        }
+    }
+
+    else { println!("pattern match unsuccessfull"); }
 }
